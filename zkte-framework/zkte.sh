7fd26fbdc61b53513e1e23741a3c4def82d8e721
Daniel Moreno <irvind.moreno@hotmail.com>
2015-11-09 23:17:40 -0500

    04

--- zkte-framework/zkte.sh
+++ zkte-framework/zkte.sh
@@ -1,20 +1,85 @@
 #!/bin/bash
-function crearModulo
+function crearLayoutSinAngular
+{
+echo 'html()
+	head
+		meta(charset="utf-8")
+		meta(name="viewport" content="width=device-width, user-scalable=no")
+		link(href="../layout/layout.css" rel="stylesheet")
+		script(src="../js/clases.js")
+		script(src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
+		link(rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css")
+		block head
+	body		
+		block header
+		block navegacion
+		block contenido			
+		block footer' >> layout.jade
+}
+function crearProyecto
 {
 	read -p 'Nombre Del Proyecto: ' nombreDelProyecto
-	read -p 'Nombre Del Componente: ' nombreComponente
-	cd proyecto
-	sudo mkdir $nombreDelProyecto
-	cd $nombreDelProyecto
-	sudo mkdir $nombreComponente
-	cd $nombreComponente
-	sudo touch $nombreComponente".jade"
-	sudo touch $nombreComponente".styl"
-	sudo touch $nombreComponente".js"
-	cd ..
-	cd ..
-	cd ..	
-	echo $nombreComponente	
+	#read -p 'Escriba su usuario de: ' nombreDelProyecto
+	#pwd
+	rutaInicial=$(pwd)
+	rutaProyecto="$(pwd)/proyecto/$nombreDelProyecto"
+	#echo  $ruta
+	if [ -d $rutaProyecto ];
+	then	
+		echo "Ya Existe un proyecto con este nombre"	
+	else
+		#cd proyecto
+		rutaLayout="$rutaProyecto/layout"
+		mkdir $rutaProyecto
+		mkdir $rutaLayout
+		touch "$rutaLayout/layout.jade"
+		touch "$rutaLayout/layout.styl"
+		cd $rutaLayout
+		echo "Creando Layout"
+		crearLayoutSinAngular
+		echo "Layout creado correctamente"
+		echo "Creando Prmiera Vista"
+		cd $rutaProyecto
+		crearModulo
+		echo "Vista creado correctamente"
+		cd $rutaInicial
+		#cd
+		echo "Se creo el proyecto"
+	fi
 }
-crearModulo
+function crearJade
+{
+	read -p 'Componente Header: ' componenteHeader
+	rutaComponentes="$rutaInicial/componentes/header/$componenteHeader"
+	if [ -d $rutaComponentes ];
+	then
+		echo "si existe este componente"
+		includeHeader="include ../../../componentes/header/$componenteHeader/header.jade"
+	else
+		includeHeader=""
+		echo "no existe este componente"
+	fi
+	touch $nombreDeLaVista".jade"
+echo "extends ../layout/layout.jade
+block head
+	link(href='./$nombreDeLaVista.css' rel='stylesheet')
+	script(src='./$nombreDeLaVista.js')
+block header
+	- rutaImagenes ='../../../imagenes';
+	$includeHeader
+block navegacion	
+block contenido	
+block footer" >> $nombreDeLaVista.jade
+}
+function crearModulo
+{	
+	read -p 'Nombre la Primera Vista: ' nombreDeLaVista
+	mkdir $nombreDeLaVista
+	rutaVista="$rutaProyecto/$nombreDeLaVista"	
+	cd $rutaVista
+	crearJade		
+	touch $nombreDeLaVista".styl"
+	touch $nombreDeLaVista".js"	
+}
+crearProyecto
 
