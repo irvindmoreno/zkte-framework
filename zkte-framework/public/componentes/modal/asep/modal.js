b325473e93ff3bb38d0b80334b0a14171882e237
Samuel <samuel_1710@hotmail.com>
2015-11-20 12:16:42 -0500

    cambios

---
+++ zkte-framework/public/componentes/modal/asep/modal.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ModalAsep = function ModalAsep() {
+	_classCallCheck(this, ModalAsep);
+};
+
+//alert("kjasdhjdjkasd");
+
+var a = new ModalAsep();
