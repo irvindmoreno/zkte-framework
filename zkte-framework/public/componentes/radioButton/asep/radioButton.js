bf5d59e20ad3741ec42b8ac269b8222b7418dd37
Daniel Moreno <irvind.moreno@hotmail.com>
2015-11-30 10:07:25 -0500

    github pages

---
+++ zkte-framework/public/componentes/radioButton/asep/radioButton.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var RadioButtonAsep = function RadioButtonAsep() {
+	_classCallCheck(this, RadioButtonAsep);
+};
